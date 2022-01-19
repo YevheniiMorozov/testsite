86a4aee0311b315305de0b8e66a345a2c9c9598c
YevheniiMorozov <lvonKrisp@gmail.com>
2022-01-19 17:08:29 +0200

    test site for studying

---
+++ mysite/news/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import News
+
+# Настройка формы публикации новости
+
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        # fields = '__all__'
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
---
+++ mysite/news/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.urls import reverse
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Наименование")  # Заголовок, поле обязательное
+    content = models.TextField(blank=True, verbose_name="Контент")  # не обязательно заполнять
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")  # Время создания
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлено")      # Время редактирования
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото", blank=True)  # Загрузить фото в этот путь
+    is_published = models.BooleanField(default=True, verbose_name="Опубликовано")  # Подтверждение публикации
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={'news_id': self.pk})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+        ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name="Категория")
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'category_id': self.pk})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['title']
